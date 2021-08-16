da232c06db962089f442725167d1187f91ced9fa
Bront <Brentholt_1@outlook.com>
2021-08-16 00:15:41 -0400

    Update audit.py

--- audit/audit.py
+++ audit/audit.py
@@ -1165,7 +1165,7 @@ class Audit(commands.Cog):
         if invite.max_age == 0:
             inv_text = 'Never'
         else:
-            inv_text = human_timedelta(relativedelta(seconds=invite.max_age))
+            inv_text = human_timedelta(relativedelta(microseconds=invite.max_age))
         embed.add_field(name="Expires after", value=inv_text)
         embed.add_field(name="Max uses", value="Unlimited" if invite.max_age == 0 else str(invite.max_age))
         if invite.temporary:
