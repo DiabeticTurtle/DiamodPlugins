314f584ff6a3cf217a76a5efbd763e712c58e344
Bront <Brentholt_1@outlook.com>
2021-09-25 23:30:05 -0400

    Revert "Update moderation.py"
    This reverts commit 5a0c913ff4b4165f23f20477ae013bbc54f89115.

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -340,72 +340,7 @@ class Moderation(commands.Cog):
                 color=self.bot.main_color,
             ).set_footer(text=f"This is the {case} case.")
         )
-    @commands.command(usage="<Member> <time> [reason]")
-    @checks.has_permissions(PermissionLevel.MODERATOR)
-    async def tempmute(self, ctx, member:discord.Member, *, time:TimeConverter=None, reason=None):
-        """Mutes a member for the specified time- time in 2d 10h 3m 2s format ex:
-        &mute @Someone 1d"""
-        print(1)
-        if time == None:
-            embed = discord.Embed(
-                title= "Error",
-                description= "Please specify a time",
-                color= 0xFF0000
-            )
-            await ctx.send(embed=embed)
-            print(2)
-        if member == None:
-            embed = discord.Embed(
-                title= "Error",
-                description= "Please specify a member to mute",
-                color= 0xFF0000
-            )
-        if reason != None:
-            if not reason.endswith("."):
-                reason = reason + "."
 
-        case = await self.get_case()
-
-        msg = f"You have been warned in {ctx.guild.name}" + (
-            f" for: {reason}" if reason else "."
-        )
-        await ctx.send(embed=embed)
-        print(3)
-    else:
-        role = discord.utils.get(ctx.guild.roles, name="Muted")
-        if role == None:
-            role = await ctx.guild.create_role(name="Muted")
-            for channel in ctx.guild.text_channels:                    
-            await channel.set_permissions(role, send_messages=False)
-            await member.add_roles(role)
-            embed = discord.Embed(
-                title= "Mute",
-                description= f"{member.mention} has been muted by {ctx.message.author.mention} for {time}s",
-                color=0x00FF00
-            )
-            await ctx.send(embed=embed)
-            print(4)
-            embed = discord.Embed(
-                title= "Muted",
-                description= f"You have been muted in {ctx.guiild.name} by {ctx.author.mention} for {time}",
-                color=0x06c9ff
-            )      
-            await member.send(embed=embed)
-            print(5)
-        if time:
-            await asyncio.sleep(time)
-            await member.remove_roles(role)
-            print(6)
-             
-    @tempmute.error
-    async def tempmute_error(self, ctx, error):
-        if isinstance(error, commands.MissingPermissions):
-            embed = discord.Embed(
-                title="Error",
-                description="You do not have permissions to tempmute members!",
-                color=0xFF0000
-            )
-            await ctx.send(embed=embed)
     @commands.command(usage="<Member> [reason]")
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def unmute(self, ctx, member: discord.Member = None, *, reason=None):
