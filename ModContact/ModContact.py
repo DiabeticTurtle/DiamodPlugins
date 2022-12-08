c5d8d88b6113551e1a1fd19bfc2758711ea6d661
Bront <Brentholt_1@outlook.com>
2022-12-08 18:42:21 -0500

    Update ModContact.py

--- ModContact/ModContact.py
+++ ModContact/ModContact.py
@@ -7,7 +7,8 @@ class ModContact(commands.Cog):
     # Set the bot's status to let people know they can DM it to contact the mods
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.bot.change_presence(status=discord.Status.online, activity=discord.Game("DM me to contact the mods"))
+        activity = discord.Activity(name="DM me to contact the mods", type=discord.ActivityType.listening)
+        await self.bot.change_presence(status=discord.Status.online, activity=activity)
 
     @commands.command()
     async def join(self, ctx, thread_id: int):
