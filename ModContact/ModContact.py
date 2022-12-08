2a7ed7328098ea0e156a4a0cc277713ee435e05f
Bront <Brentholt_1@outlook.com>
2022-12-08 18:33:42 -0500

    Create ModContact.py

---
+++ ModContact/ModContact.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+class ModContact(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Set the bot's status to let people know they can DM it to contact the mods
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(status=discord.Status.online, activity=discord.Game("DM me to contact the mods"))
+
+    @commands.command()
+    async def join(self, ctx, thread_id: int):
+            if ctx.message.is_a_thread():
+                await ctx.message.channel.join_thread(thread_id)
+
+async def setup(bot):
+    await bot.add_cog(ModContact(bot))
