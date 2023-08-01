7eefe1145137029bdde042c6a4f3d71be8d6c0bd
Bront <Brentholt_1@outlook.com>
2023-08-01 18:49:09 -0400

    Update tags.py

--- tags/tags.py
+++ tags/tags.py
@@ -1,16 +1,19 @@
+import json
+from typing import Any, Dict, Union
+
 import discord
 from datetime import datetime
 from discord.ext import commands
+from box import Box
 from core import checks
 from core.models import PermissionLevel
-import json
-from typing import Any, Dict, Union
-from box import Box
 from .models import apply_vars, SafeString
+
 
 class TagSelectMenu(discord.ui.View):
     def __init__(self, tags):
         super().__init__()
+        self.tags = tags
         self.add_item(discord.ui.Select(
             placeholder='Select a tag category...',
             options=[discord.SelectOption(label=tag, value=tag) for tag in tags]
@@ -21,7 +24,7 @@ class TagsPlugin(commands.Cog):
         self.bot: discord.Client = bot
         self.db = bot.plugin_db.get_partition(self)
         self.categories = set()  # A set to store unique tag categories
-
+        
     @commands.group(invoke_without_command=True)
     @commands.guild_only()
     @checks.has_permissions(PermissionLevel.REGULAR)
@@ -65,11 +68,35 @@ class TagsPlugin(commands.Cog):
         )
         return
 
-    
+    @tags.command()
+    async def add(self, ctx: commands.Context, name: str, *, content: str):
+        """
+        Make a new tag
+        """
+        if (await self.find_db(name=name)) is not None:
+            await ctx.send(f":x: | Tag with name `{name}` already exists!")
+            return
+        else:
+            ctx.message.content = content
+            await self.db.insert_one(
+                {
+                    "name": name,
+                    "content": ctx.message.clean_content,
+                    "createdAt": datetime.utcnow(),
+                    "updatedAt": datetime.utcnow(),
+                    "author": ctx.author.id,
+                    "uses": 0,
+                }
+            )
 
+            await ctx.send(
+                f":white_check_mark: | Tag with name `{name}` has been successfully created!"
+            )
+            return
+        
     @tags.command(name='list')
     async def list_(self, ctx):
-        '''Get a list of tags that have already been made.'''
+        '''Get a list of tags that hace already been made.'''
 
         tags = await self.db.find({}).to_list(length=None)
 
@@ -91,43 +118,146 @@ class TagsPlugin(commands.Cog):
 
         # Send the embed object
         await ctx.send(embed=embed)
+   
 
-    # ... (your other commands)
+
 
     @tags.command()
-    async def select(self, ctx: commands.Context):
-        '''
-        Select a tag category from the dropdown menu.
-        '''
+    async def edit(self, ctx: commands.Context, name: str, *, content: str):
+        """
+        Edit an existing tag
+        Only owner of tag or user with Manage Server permissions can use this command
+        """
+        tag = await self.find_db(name=name)
 
-        tags = await self.db.find({}).to_list(length=None)
+        if tag is None:
+            await ctx.send(f":x: | Tag with name `{name}` does'nt exist")
+            return
+        else:
+            member: discord.Member = ctx.author
+            if ctx.author.id == tag["author"] or member.guild_permissions.manage_guild:
+                await self.db.find_one_and_update(
+                    {"name": name},
+                    {"$set": {"content": content, "updatedAt": datetime.utcnow()}},
+                )
 
-        if tags is None:
-            return await ctx.send(':x: | You don\'t have any tags.')
+                await ctx.send(
+                    f":white_check_mark: | Tag `{name}` is updated successfully!"
+                )
+            else:
+                await ctx.send("You don't have enough permissions to edit that tag")
 
-        # Create and send the dropdown menu
-        view = TagSelectMenu([tag['name'] for tag in tags])
-        await ctx.send('Select a tag category:', view=view)
+    @tags.command()
+    async def delete(self, ctx: commands.Context, name: str):
+        """
+        Delete a tag.
+        Only owner of tag or user with Manage Server permissions can use this command
+        """
+        tag = await self.find_db(name=name)
+        if tag is None:
+            await ctx.send(f":x: | Tag `{name}` not found in the database.")
+        else:
+            if (
+                ctx.author.id == tag["author"]
+                or ctx.author.guild_permissions.manage_guild
+            ):
+                await self.db.delete_one({"name": name})
 
-        # Wait for the user to make a selection
-        interaction = await self.bot.wait_for('select_option', check=lambda i: i.user == ctx.author and i.component == view.children[0])
+                await ctx.send(
+                    f":white_check_mark: | Tag `{name}` has been deleted successfully!"
+                )
+            else:
+                await ctx.send("You don't have enough permissions to delete that tag")
 
-        selected_tag = interaction.values[0]
-        await ctx.send(f'You selected the tag category: {selected_tag}')
+    @tags.command()
+    async def claim(self, ctx: commands.Context, name: str):
+        """
+        Claim a tag if the user has left the server
+        """
+        tag = await self.find_db(name=name)
+
+        if tag is None:
+            await ctx.send(f":x: | Tag `{name}` not found.")
+        else:
+            member = await ctx.guild.get_member(tag["author"])
+            if member is not None:
+                await ctx.send(
+                    f":x: | The owner of the tag is still in the server `{member.name}#{member.discriminator}`"
+                )
+                return
+            else:
+                await self.db.find_one_and_update(
+                    {"name": name},
+                    {"$set": {"author": ctx.author.id, "updatedAt": datetime.utcnow()}},
+                )
+
+                await ctx.send(
+                    f":white_check_mark: | Tag `{name}` is now owned by `{ctx.author.name}#{ctx.author.discriminator}`"
+                )
+
+    @tags.command()
+    async def info(self, ctx: commands.Context, name: str):
+        """
+        Get info on a tag
+        """
+        tag = await self.find_db(name=name)
+
+        if tag is None:
+            await ctx.send(f":x: | Tag `{name}` not found.")
+        else:
+            user: discord.User = await self.bot.fetch_user(tag["author"])
+            embed = discord.Embed()
+            embed.colour = discord.Colour.green()
+            embed.title = f"{name}'s Info"
+            embed.add_field(
+                name="Created By", value=f"{user.name}#{user.discriminator}"
+            )
+            embed.add_field(name="Created At", value=tag["createdAt"])
+            embed.add_field(
+                name="Last Modified At", value=tag["updatedAt"], inline=False
+            )
+            embed.add_field(name="Uses", value=tag["uses"], inline=False)
+            await ctx.send(embed=embed)
+            return
+
+    @commands.command()
+    async def tag(self, ctx: commands.Context, name: str):
+        
+        """
+        Use a tag!
+        """
+        tag = await self.find_db(name=name)
+        if tag is None:
+            await ctx.send(f":x: | Tag {name} not found.")
+            return
+        else:
+            await ctx.send("```" + tag["content"] + "```")
+            await self.db.find_one_and_update(
+                {"name": name}, {"$set": {"uses": tag["uses"] + 1}}
+            )
+            return
 
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message):
+        if msg.content.startswith("Please set your Nightscout") and msg.author.bot:
+            await ctx.send("If you'd like to learn more about Nightscout, type `?nightscout`.")
+            return
         if not msg.content.startswith(self.bot.prefix) or msg.author.bot:
             return
-
+        
         content = msg.content.replace(self.bot.prefix, "")
         names = content.split(" ")
 
         tag = await self.db.find_one({"name": names[0]})
+        thing = json.loads(tag["content"])
+        embed = discord.Embed.from_dict(thing['embed'])
         if tag is None:
             return
         else:
-            await msg.channel.send("```" + tag["content"] + "```")
+            
+            
+            
+            await msg.channel.send(embed=embed)
             await self.db.find_one_and_update(
                 {"name": names[0]}, {"$set": {"uses": tag["uses"] + 1}}
             )
@@ -135,6 +265,24 @@ class TagsPlugin(commands.Cog):
 
     async def find_db(self, name: str):
         return await self.db.find_one({"name": name})
+
+    #def format_message(self, tag: str, message: discord.Message) -> Dict[str, Union[Any]]:
+    #    updated_tag: Dict[str, Union[Any]]
+    #    try:
+    #        updated_tag = json.loads(tag)
+    #    except json.JSONDecodeError:
+    #        # message is not embed
+    #        tag = apply_vars(self.bot, tag, message)
+    #        updated_tag = {'content': tag}
+    #    else:
+    #        # message is embed
+    #        updated_tag = self.apply_vars_dict(updated_tag, message)
+
+    #        if 'embed' in updated_tag:
+    #            updated_tag['embed'] = discord.Embed.from_dict(updated_tag['embed'])
+    #        else:
+    #            updated_tag = None
+    #    return updated_tag
 
 
 async def setup(bot):
