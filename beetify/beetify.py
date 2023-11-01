e896c9ccd7ade71fe8ba43adccf00e54bf80b613
Bront <Brentholt_1@outlook.com>
2023-11-01 18:38:54 -0400

    Update beetify.py

--- beetify/beetify.py
+++ beetify/beetify.py
@@ -46,7 +46,7 @@ class Beetify(commands.Cog):
 
             draw = ImageDraw.Draw(avatar_image)
 
-            circle_radius = 100
+            circle_radius = 24
             num_colors = 360  # Number of colors in the rainbow (360 degrees)
 
             for angle in range(num_colors):
