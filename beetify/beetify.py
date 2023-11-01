f1fdef58eb7220a6598b06be97300cb09a1bc731
Bront <Brentholt_1@outlook.com>
2023-11-01 14:39:20 -0400

    Update beetify.py

--- beetify/beetify.py
+++ beetify/beetify.py
@@ -1,38 +1,38 @@
 from discord.ext import commands
-from discord import File, Member
-from io import BytesIO
-from PIL import Image, ImageFont, ImageDraw, ImageOps
+from discord import File
+import io
+from PIL import Image, ImageDraw
 
-class beetify(commands.Cog):
+class Beetify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def beetify(self, ctx, member: Member = None):
-        """Add a thick blue circle (Diabetes Awareness Month style) around a user's profile picture"""
+    async def beetify(self, ctx):
+        """Add a thicker blue circle (Diabetes Awareness Month style) around your profile picture"""
+        
+        user_avatar = ctx.author.avatar.with_size(128)  
 
-        user = member if member else ctx.author
-        user_avatar = user.avatar.url
+        
+        with io.BytesIO(await user_avatar.read()) as image_binary:
+            avatar_image = Image.open(image_binary)
 
-        pfp = Image.open(BytesIO(await user_avatar.read()))
-        pfp = pfp.resize((125, 125))
+            
+            draw = ImageDraw.Draw(avatar_image)
 
-        my_image = Image.new("RGB", (512, 512))
-        my_image.paste(pfp, (36, 80))
+            
+            circle_color = "#465cec"
 
-        draw = ImageDraw.Draw(my_image)
-        circle_color = "#465cec"
-        circle_width = 24
+            
+            draw.ellipse((0, 0, 128, 128), outline=circle_color, width=14)  
 
-        draw.ellipse((0, 0, 128, 128), outline=circle_color, width=circle_width)
+            
+            with io.BytesIO() as output_binary:
+                avatar_image.save(output_binary, format="PNG")
+                output_binary.seek(0)
 
-        # You can add text or other modifications here if needed
-
-        with BytesIO() as output_binary:
-            my_image.save(output_binary, format="PNG")
-            output_binary.seek(0)
-
-            await ctx.send(file=File(output_binary, filename="beetified_avatar.png"))
+                # Send the modified avatar as a file
+                await ctx.send(file=File(output_binary, filename="beetified_avatar.png"))
 
 async def setup(bot):
-    await bot.add_cog(beetify(bot))
+    await bot.add_cog(Beetify(bot))
