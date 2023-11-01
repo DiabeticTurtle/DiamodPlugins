433b7e811ab8a0987b3d55e6a38743e877aaf79e
Bront <Brentholt_1@outlook.com>
2023-11-01 13:24:20 -0400

    change color

--- beetify/beetify.py
+++ beetify/beetify.py
@@ -20,7 +20,7 @@ class beetify(commands.Cog):
            
             draw = ImageDraw.Draw(avatar_image)
 
-            circle_color = "#79addc"
+            circle_color = "#457aed"
            
             draw.ellipse((0, 0, 128, 128), outline=circle_color, width=14)
 
