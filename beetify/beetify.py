214862c799017ec692a35e34f8483c658677e623
Bront <Brentholt_1@outlook.com>
2023-11-01 14:36:17 -0400

    Update beetify.py

--- beetify/beetify.py
+++ beetify/beetify.py
@@ -14,26 +14,25 @@ class beetify(commands.Cog):
         user = member if member else ctx.author
         user_avatar = user.avatar.url
 
-        with BytesIO(await user_avatar.read()) as data:
-            pfp = Image.open(data)
-            pfp = pfp.resize((125, 125))
+        pfp = Image.open(BytesIO(await user_avatar.read()))
+        pfp = pfp.resize((125, 125))
 
-            my_image = Image.new("RGB", (512, 512))
-            my_image.paste(pfp, (36, 80))
+        my_image = Image.new("RGB", (512, 512))
+        my_image.paste(pfp, (36, 80))
 
-            draw = ImageDraw.Draw(my_image)
-            circle_color = "#465cec"
-            circle_width = 24
+        draw = ImageDraw.Draw(my_image)
+        circle_color = "#465cec"
+        circle_width = 24
 
-            draw.ellipse((0, 0, 128, 128), outline=circle_color, width=circle_width)
+        draw.ellipse((0, 0, 128, 128), outline=circle_color, width=circle_width)
 
-            # You can add text or other modifications here if needed
+        # You can add text or other modifications here if needed
 
-            with BytesIO() as output_binary:
-                my_image.save(output_binary, format="PNG")
-                output_binary.seek(0)
+        with BytesIO() as output_binary:
+            my_image.save(output_binary, format="PNG")
+            output_binary.seek(0)
 
-                await ctx.send(file=File(output_binary, filename="beetified_avatar.png"))
+            await ctx.send(file=File(output_binary, filename="beetified_avatar.png"))
 
 async def setup(bot):
     await bot.add_cog(beetify(bot))
