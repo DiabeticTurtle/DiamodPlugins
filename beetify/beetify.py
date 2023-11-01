ea53b8e27010901bedcf7897757a7444cd77f785
Bront <Brentholt_1@outlook.com>
2023-11-01 14:30:18 -0400

    Update beetify.py

--- beetify/beetify.py
+++ beetify/beetify.py
@@ -1,48 +1,36 @@
 from discord.ext import commands
 from discord import File
-import io
-from PIL import Image, ImageDraw
+from io import BytesIO
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 
 class beetify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.avatar_size_threshold = 512
 
     @commands.command()
-    async def beetify(self, ctx):
-        """Add a thick blue circle (Diabetes Awareness Month style) around your profile picture"""
-        
-        user_avatar = ctx.author.avatar.with_size(128)
-        user_avatar_512 = ctx.author.avatar.with_size(512)
-        ellipse_dimensions = (0, 0, 0, 0)
-        circle_width = 0
+    async def beetify(self, ctx, member: Member = None):
+        """Add a thick blue circle (Diabetes Awareness Month style) around a user's profile picture"""
 
-        if user_avatar == user_avatar:
-            # Use the 512-pixel version with a width of 39
-            user_avatar = user_avatar_512
-            ellipse_dimensions = (0, 0, 512, 512)
-            circle_width = 39   
-        else:
-            # Use the 128-pixel version with a width of 24
-            ellipse_dimensions = (0, 0, 128, 128)
-            circle_width = 15
-            
+        user = member if member else ctx.author
+        user_avatar = user.avatar_url_as(size=128)
 
+        with BytesIO(await user_avatar.read()) as data:
+            pfp = Image.open(data)
+            pfp = pfp.resize((125, 125))
 
-        with io.BytesIO(await user_avatar.read()) as image_binary:
-            avatar_image = Image.open(image_binary)
+            my_image = Image.new("RGB", (512, 512))
+            my_image.paste(pfp, (36, 80))
 
-            draw = ImageDraw.Draw(avatar_image)
-
+            draw = ImageDraw.Draw(my_image)
             circle_color = "#465cec"
+            circle_width = 24
 
-            draw.ellipse(ellipse_dimensions, outline=circle_color, width=circle_width)
+            draw.ellipse((0, 0, 128, 128), outline=circle_color, width=circle_width)
 
-            with io.BytesIO() as output_binary:
-                avatar_image.save(output_binary, format="PNG")
-                output_binary.seek(0)
+            # You can add text or other modifications here if needed
 
-                await ctx.send(file=File(output_binary, filename="beetified_avatar.png"))
+
+            await ctx.send(file=File(output_binary, filename="beetified_avatar.png"))
 
 async def setup(bot):
    await bot.add_cog(beetify(bot))
