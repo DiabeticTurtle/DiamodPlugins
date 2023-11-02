34e067c204562c5bb941bea180eb770b7de7d940
Bront <Brentholt_1@outlook.com>
2023-11-01 21:49:01 -0400

    Update beetify.py

--- beetify/beetify.py
+++ beetify/beetify.py
@@ -22,13 +22,10 @@ class Beetify(commands.Cog):
             draw = ImageDraw.Draw(avatar_image)
 
             circle_color = "#465cec"
-            circle_image = Image.new("RGB", (128, 128), color=circle_color)
-
-            circle_image = circle_image.resize((128*4, 128*4), resample=Image.LANCZOS)
-            draw = ImageDraw.Draw(circle_image)
-            draw.ellipse((0, 0, 128*4, 127*4), outline=circle_color, width=14)  
-            circle_image = circle_image.resize((128, 128), resample=Image.LANCZOS)  
-            avatar_image.paste(circle_image, (14, 14))  
+            avatar_image = avatar_image.resize((128*4, 128*4), resample=Image.LANCZOS)
+            draw = ImageDraw.Draw(avatar_image)
+            draw.ellipse((0, 0, 128*4, 127*4), outline=circle_color, width=14)
+            avatar_image = avatar_image.resize((128, 128), resample=Image.LANCZOS)
 
             
             with io.BytesIO() as output_binary:
